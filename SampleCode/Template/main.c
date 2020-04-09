2e0c09727a7895da31d0e295ecfa5c2675a921b4
released <released@users.noreply.github.com>
2020-04-09 13:14:09 +0800

    Initial commit

---
+++ SampleCode/Template/main.c
@@ -0,0 +1,187 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * @brief    A project template for M031 MCU.
+ *
+ * Copyright (C) 2017 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+#include "NuMicro.h"
+
+typedef enum{
+	flag_RX_Finish = 0 ,
+	
+	flag_DEFAULT	
+}Flag_Index;
+
+
+uint8_t BitFlag = 0;
+#define BitFlag_ON(flag)							(BitFlag|=flag)
+#define BitFlag_OFF(flag)							(BitFlag&=~flag)
+#define BitFlag_READ(flag)							((BitFlag&flag)?1:0)
+#define ReadBit(bit)								(uint8_t)(1<<bit)
+
+#define is_flag_set(idx)							(BitFlag_READ(ReadBit(idx)))
+#define set_flag(idx,en)							( (en == 1) ? (BitFlag_ON(ReadBit(idx))) : (BitFlag_OFF(ReadBit(idx))))
+
+
+int IsDebugFifoEmpty(void);
+
+
+void UARTx_Process(void)
+{
+	uint8_t res = 0;
+	
+	res = UART_READ(UART0);
+
+	if (res == 'x' || res == 'X')
+	{
+		NVIC_SystemReset();
+	}
+
+	if (res > 0x7F)
+	{
+		printf("invalid command\r\n");
+	}
+	else
+	{
+		switch(res)
+		{
+			case '1':
+
+				break;				
+		}
+	}
+}
+
+void UART02_IRQHandler(void)
+{
+    if(UART_GET_INT_FLAG(UART0, UART_INTSTS_RDAINT_Msk | UART_INTSTS_RXTOINT_Msk))     /* UART receive data available flag */
+    {
+        while(UART_GET_RX_EMPTY(UART0) == 0)
+        {
+            UARTx_Process();
+        }
+    }
+
+    if(UART0->FIFOSTS & (UART_FIFOSTS_BIF_Msk | UART_FIFOSTS_FEF_Msk | UART_FIFOSTS_PEF_Msk | UART_FIFOSTS_RXOVIF_Msk))
+    {
+        UART_ClearIntFlag(UART0, (UART_INTSTS_RLSINT_Msk| UART_INTSTS_BUFERRINT_Msk));
+    }
+}
+
+void UART0_Init(void)
+{
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+
+	/* Set UART receive time-out */
+	UART_SetTimeoutCnt(UART0, 20);
+
+	UART0->FIFO &= ~UART_FIFO_RFITL_4BYTES;
+	UART0->FIFO |= UART_FIFO_RFITL_8BYTES;
+
+	/* Enable UART Interrupt - */
+	UART_ENABLE_INT(UART0, UART_INTEN_RDAIEN_Msk | UART_INTEN_TOCNTEN_Msk | UART_INTEN_RXTOIEN_Msk);
+	
+	NVIC_EnableIRQ(UART02_IRQn);
+
+	printf("\r\nCLK_GetCPUFreq : %8d\r\n",CLK_GetCPUFreq());
+	printf("CLK_GetHXTFreq : %8d\r\n",CLK_GetHXTFreq());
+	printf("CLK_GetLXTFreq : %8d\r\n",CLK_GetLXTFreq());	
+	printf("CLK_GetPCLK0Freq : %8d\r\n",CLK_GetPCLK0Freq());
+	printf("CLK_GetPCLK1Freq : %8d\r\n",CLK_GetPCLK1Freq());	
+}
+
+void TMR1_IRQHandler(void)
+{
+	static uint32_t LOG = 0;
+	static uint16_t CNT = 0;
+	
+    if(TIMER_GetIntFlag(TIMER1) == 1)
+    {
+        TIMER_ClearIntFlag(TIMER1);
+
+		if (CNT++ >= 1000)
+		{		
+			CNT = 0;
+        	printf("%s : %4d\r\n",__FUNCTION__,LOG++);
+			PB14 ^= 1;	
+
+		}		
+    }
+}
+
+void TIMER1_Init(void)
+{
+    TIMER_Open(TIMER1, TIMER_PERIODIC_MODE, 1000);
+    TIMER_EnableInt(TIMER1);
+    NVIC_EnableIRQ(TMR1_IRQn);	
+    TIMER_Start(TIMER1);
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable HIRC clock (Internal RC 48MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+//    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+	
+    /* Wait for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+//    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+	
+    /* Select HCLK clock source as HIRC and HCLK source divider as 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Enable UART0 clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_PCLK0, CLK_CLKDIV0_UART0(1));
+	
+    CLK_EnableModuleClock(TMR1_MODULE);
+    CLK_SetModuleClock(TMR1_MODULE, CLK_CLKSEL1_TMR1SEL_PCLK0, 0);
+//    CLK_SetModuleClock(TMR1_MODULE, CLK_CLKSEL1_TMR1SEL_HIRC, 0);
+	
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk))    |       \
+                    (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*
+ * This is a template project for M031 series MCU. Users could based on this project to create their
+ * own application without worry about the IAR/Keil project settings.
+ *
+ * This template application uses external crystal as HCLK source and configures UART0 to print out
+ * "Hello World", users may need to do extra system configuration based on their system design.
+ */
+
+int main()
+{
+    SYS_Init();
+
+    UART0_Init();
+	
+	TIMER1_Init();
+	
+    GPIO_SetMode(PB, BIT14, GPIO_MODE_OUTPUT);
+	
+    /* Got no where to go, just loop forever */
+    while(1)
+    {
+		
+    }
+}
+
+/*** (C) COPYRIGHT 2017 Nuvoton Technology Corp. ***/
